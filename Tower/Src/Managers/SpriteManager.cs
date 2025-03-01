1c489ba7715b8c3c18a478f221a675d67df127d6
Pavel <ppovolyaev@inbox.ru>
2025-03-01 19:58:36 +0300

    more texture

--- Tower/Src/Managers/SpriteManager.cs
+++ Tower/Src/Managers/SpriteManager.cs
@@ -6,9 +6,15 @@ namespace Tower.Managers;
 public class SpriteManager
 {
     public Texture2D BaseEnemySprite { get; set; }
+    public Texture2D CiradelSprite { get; set; }
+    public Texture2D TowerSprite { get; set; }
+    public Texture2D WallSprite { get; set; }
 
     public void LoadSprites(ContentManager contentManager)
     {
         BaseEnemySprite = contentManager.Load<Texture2D>("monster");
+        CiradelSprite = contentManager.Load<Texture2D>("citadel");
+        TowerSprite = contentManager.Load<Texture2D>("tower");
+        WallSprite = contentManager.Load<Texture2D>("wall");
     }
 }
