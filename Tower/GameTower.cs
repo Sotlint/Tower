1aeaf2d77a50bc11b1ce599265f0d546625ee488
Pavel <ppovolyaev@inbox.ru>
2025-05-17 23:00:03 +0300

    уборка

--- Tower/GameTower.cs
+++ Tower/GameTower.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Tower.LogicUpdaters;
 using Tower.Managers;
--- Tower/Src/LogicUpdaters/PlannedUpdateLogic.cs
+++ Tower/Src/LogicUpdaters/PlannedUpdateLogic.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Xna.Framework;
 using Tower.Core.Abstractions.Enums;
 using Tower.Managers;
